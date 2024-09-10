bc16596241039052e03ac64fe20171832f005b98
kiranmai2606 <50226540+kiranmai2606@users.noreply.github.com>
2024-09-10 11:06:31 +0530

    update to Task 6 ..Added string array  reverse

--- src/tasks/Task6.java
+++ src/tasks/Task6.java
@@ -6,19 +6,36 @@ public class Task6 {
 	{
 		//one dimensional array
 		int arr[]= {10,20,30,40,50};
-		System.out.println(arr.length);
+		String str[]= {"kiranmai","Tenali","522201","AK"};
+	
+		System.out.println("Integer array length" +arr.length);	//Finding length of an array
+		System.out.println("string array length"  +str.length);	//Finding length of an array
 		
-		System.out.println("Elements in array ");//Finding length of an array 
+		System.out.println("Elements in Interger array ");
+ 
 		 for (int i=0;i<arr.length ;i++)
 		  {
 			 
-			  System.out.println(+arr[i]);// For printing array elements 
+			  System.out.print(arr[i] + " ");// For printing array elements 
 		  }
-		 System.out.println("Reverse array ");
+		 System.out.println("\nReverse Integer array ");
 		 for(int j=arr.length-1 ;j>=0; j--)
 		 {
 		  // Reverse array elements 
-		  System.out.println(arr[j]);
+			 System.out.print(arr[j] + " ");
+		 }  
+		System.out.println("\nElements in string array ");
+		 for (int i=0;i<str.length ;i++)
+		  {
+			 
+			 System.out.print(str[i] + " ");// For printing array elements 
+		  }
+		
+		 System.out.println("\nReverse String array ");
+		 for(int j=str.length-1 ;j>=0; j--)
+		 {
+		  // Reverse array elements 
+			 System.out.print(str[j] + " ");
 		 }  
 	}
 
